64fab8fcd1700bb45b48edd9d1b52b9bc0afcea7
parshva555 <parshvadani@gmail.com>
2024-04-02 11:46:28 +0530

    Home Screen header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,38 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { Ionicons } from "@expo/vector-icons";
+import Header from '../components/Header';
 const HomeScreen = () => {
+  const navigation = useNavigation();
+  useLayoutEffect(()=> {
+    navigation.setOptions({
+      headerShown:true,
+      title:"Booking.com",
+      headerTitleStyle:{
+        fontSize:20,
+        fontWeight: 'bold',
+        color:'white',
+        marginLeft:100
+      },
+      headerStyle:{
+        backgroundColor:'#003580',
+        height:110,
+      },
+      headerRight:()=> (
+        <Ionicons
+                  name="notifications-outline"
+                  size={24}
+                  color="white"
+                  style={{marginRight:12}}
+                  
+                />
+      )
+    })
+  },[])
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <Header/>
     </View>
   )
 }
