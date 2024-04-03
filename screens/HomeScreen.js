d608b4fd57fc0a0820122655db59eeaa1643006c
parshva555 <parshvadani@gmail.com>
2024-04-03 23:08:22 +0530

    Bottom Modal of rooms guests and children

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -14,11 +14,23 @@ import { Ionicons } from "@expo/vector-icons";
 import Header from "../components/Header";
 import { Feather } from "@expo/vector-icons";
 import DatePicker from "react-native-date-ranges";
+import {
+  BottomModal,
+  ModalButton,
+  ModalContent,
+  ModalFooter,
+  ModalTitle,
+  SlideAnimation,
+} from "react-native-modals";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
-  const [selectedDates,setSelectedDates] = useState()
-  console.log(selectedDates)
+  const [selectedDates, setSelectedDates] = useState();
+  const [rooms, setRooms] = useState(1);
+  const [adults, setAdults] = useState(2);
+  const [children, setChildren] = useState(0);
+  const [modalVisible, setModalVisible] = useState(false);
+  console.log(selectedDates);
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: true,
@@ -49,100 +61,178 @@ const HomeScreen = () => {
   const customButton = (onConfirm) => {
     return (
       <Button
-      onPress={onConfirm}
-      style={{
-        container:{width:"80%", marginHorizontal:"3%"},
-        text:{fontSize:20}
-      }}
-      primary
-      title="Submit"
+        onPress={onConfirm}
+        style={{
+          container: { width: "80%", marginHorizontal: "3%" },
+          text: { fontSize: 20 },
+        }}
+        primary
+        title="Submit"
       />
-    )
-  }
+    );
+  };
 
   return (
-    <View>
-      <Header />
-      <ScrollView>
-        <View
-          style={{
-            margin: 20,
-            borderColor: "#FFC72C",
-            borderWidth: 3,
-            borderRadius: 6,
-          }}
-        >
-          {/* Destination */}
-          <Pressable
+    <>
+      <View>
+        <Header />
+        <ScrollView>
+          <View
             style={{
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 10,
-              paddingHorizontal: 10,
+              margin: 20,
               borderColor: "#FFC72C",
-              borderWidth: 2,
-              paddingVertical: 15,
+              borderWidth: 3,
+              borderRadius: 6,
             }}
           >
-            <Feather name="search" size={24} color="black" />
-            <TextInput placeholder="Enter your Destination" />
-          </Pressable>
-
-          {/* Selected Dates */}
-          <Pressable
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 10,
-              paddingHorizontal: 10,
-              borderColor: "#FFC72C",
-              borderWidth: 2,
-              paddingVertical: 15,
-            }}
-          >
-            <Feather name="calendar" size={24} color="black" />
-            <DatePicker
+            {/* Destination */}
+            <Pressable
               style={{
-                width: 250,
-                height: 30,
-                borderRadius:0,
-                borderWidth:0,
-                borderColor:'transparent'
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 10,
+                paddingHorizontal: 10,
+                borderColor: "#FFC72C",
+                borderWidth: 2,
+                paddingVertical: 15,
               }}
-              customStyles={{
-                placeholderText: {
-                  fontSize: 15,
-                  flexDirection: "row",
-                  alignItems: "center",
-                  marginRight: "auto",
-                },
-                headerStyle: {
-                  backgroundColor: "#0035ab",
-                },
-                contentText: {
-                  fontSize: 15,
-                  flexDirection: "row",
-                  alignItems: "center",
-                  marginRight: "auto",
-                },
+            >
+              <Feather name="search" size={24} color="black" />
+              <TextInput
+                placeholder="Enter your Destination"
+                placeholderTextColor="black"
+              />
+            </Pressable>
+
+            {/* Selected Dates */}
+            <Pressable
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 10,
+                paddingHorizontal: 10,
+                borderColor: "#FFC72C",
+                borderWidth: 2,
+                paddingVertical: 15,
               }}
-              selectedBgColor="#0047AB"
-              customButton={(onConfirm) => customButton(onConfirm)}
-              onConfirm={(startDate,endDate) => setSelectedDates(startDate,endDate)}
-              allowFontScaling={false}
-              placeholder={"April 27, 2021 ->  April 28, 2021"}
-              mode={"range"}
+            >
+              <Feather name="calendar" size={24} color="black" />
+              <DatePicker
+                style={{
+                  width: 250,
+                  height: 30,
+                  borderRadius: 0,
+                  borderWidth: 0,
+                  borderColor: "transparent",
+                }}
+                customStyles={{
+                  placeholderText: {
+                    fontSize: 15,
+                    flexDirection: "row",
+                    alignItems: "center",
+                    marginRight: "auto",
+                  },
+                  headerStyle: {
+                    backgroundColor: "#0035ab",
+                  },
+                  contentText: {
+                    fontSize: 15,
+                    flexDirection: "row",
+                    alignItems: "center",
+                    marginRight: "auto",
+                  },
+                }}
+                selectedBgColor="#0047AB"
+                customButton={(onConfirm) => customButton(onConfirm)}
+                onConfirm={(startDate, endDate) =>
+                  setSelectedDates(startDate, endDate)
+                }
+                allowFontScaling={false}
+                placeholder={"April 27, 2021 ->  April 28, 2021"}
+                mode={"range"}
+              />
+            </Pressable>
+
+            {/* Rooms and Guests */}
+            <Pressable
+            onPress={() => setModalVisible(!modalVisible)}
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 10,
+                paddingHorizontal: 10,
+                borderColor: "#FFC72C",
+                borderWidth: 2,
+                paddingVertical: 15,
+              }}
+            >
+              <Ionicons name="person-outline" size={24} color="black" />
+              <TextInput
+                placeholderTextColor="red"
+                placeholder="1 room - 2 Adults - 0 Children"
+              />
+            </Pressable>
+
+            {/* Search Button */}
+            <Pressable
+              style={{
+                paddingHorizontal: 10,
+                borderColor: "#FFC72C",
+                borderWidth: 2,
+                paddingVertical: 15,
+                backgroundColor: "#2a52be",
+              }}
+            >
+              <Text
+                style={{
+                  textAlign: "center",
+                  fontSize: 15,
+                  fontWeight: 500,
+                  color: "white",
+                }}
+              >
+                Search
+              </Text>
+            </Pressable>
+          </View>
+        </ScrollView>
+      </View>
+
+      <BottomModal
+        swipeThreshold={200}
+        onBackdropPress={() => setModalVisible(!modalVisible)}
+        swipeDirection={["up", "down"]}
+        footer={
+          <ModalFooter>
+            <ModalButton
+              text="Apply"
+              style={{
+                marginBottom: 20,
+                color:"white",
+                backgroundColor: "#003580",
+              }}
+              onPress={() => setModalVisible(!modalVisible)}
             />
-          </Pressable>
+          </ModalFooter>
+        }
+        modalTitle={<ModalTitle title="Select rooms and guests" />}
+        modalAnimation={
+          new SlideAnimation({
+            slideFrom: "bottom",
+          })
+        }
+        onHardwareBackPress={() => setModalVisible(!modalVisible)}
+        visible={modalVisible}
+        onTouchOutside={() => setModalVisible(!modalVisible)}
+      > 
+      <ModalContent style={{
+        width:'100%',
+        height:310,
+      }}>
 
-          {/* Rooms and Guests */}
-          <Pressable></Pressable>
-
-          {/* Search Button */}
-          <Pressable></Pressable>
-        </View>
-      </ScrollView>
-    </View>
+      </ModalContent>
+      </BottomModal>
+    </>
   );
 };
 
