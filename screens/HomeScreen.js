e80edfea5200f93dee6dc5bc4c4c2346f541a5c9
parshva555 <parshvadani@gmail.com>
2024-04-06 00:51:20 +0530

    Adding Footer image and Discount Cards

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import {
   Button,
+  Image,
   Pressable,
   ScrollView,
   StyleSheet,
@@ -195,6 +196,29 @@ const HomeScreen = () => {
               </Text>
             </Pressable>
           </View>
+          <Text style={{marginHorizontal:20,fontSize:17,fontWeight:500}}>
+            Travel More and Spend Less
+          </Text>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          <Pressable style={{width:200,height:150,marginTop:10,backgroundColor:'#003580', borderRadius:10,padding:20,marginHorizontal:20}}>
+            <Text style={{color:'white',fontSize:15,fontWeight:"bold",marginVertical:7}}>Genius</Text>
+            <Text style={{color:'white',fontSize:15,fontWeight:500}}>You are at Genius Level one in our Loyalty Program</Text>
+          </Pressable>
+          <Pressable style={{width:200,height:150,marginTop:10,borderColor:'#e0e0e0',borderWidth:2, borderRadius:10,padding:20,marginHorizontal:20}}>
+            <Text style={{fontSize:15,fontWeight:"bold",marginVertical:7}}>15% Discounts</Text>
+            <Text style={{fontSize:15,fontWeight:500}}>Complete 5 Stays to unlock level 2</Text>
+          </Pressable>
+          <Pressable style={{width:200,height:150,marginTop:10,borderColor:'#e0e0e0',borderWidth:2, borderRadius:10,padding:20,marginHorizontal:10}}>
+            <Text style={{fontSize:15,fontWeight:"bold",marginVertical:7}}>10% Discounts</Text>
+            <Text style={{fontSize:15,fontWeight:500}}>Enjoy Discounts at participating at properties Worldwide</Text>
+          </Pressable>
+          </ScrollView>
+          <Pressable
+          style={{marginTop:40,justifyContent:'center',alignItems:'center'}}>
+            <Image style={{width:200,height:50,resizeMode:"cover"}} source={{
+              uri:"https://assets.stickpng.com/thumbs/5a32a821cb9a85480a628f8f.png"
+            }}/>            
+          </Pressable>
         </ScrollView>
       </View>
 
