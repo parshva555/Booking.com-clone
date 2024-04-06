a79c928f8b608c2699924e97c4b5107f5f1df3b9
parshva555 <parshvadani@gmail.com>
2024-04-06 16:27:07 +0530

    Applying sort bottom modal and adding low to high and high to low functionality for cost in PlacesScreen

--- screens/PlacesScreen.js
+++ screens/PlacesScreen.js
@@ -1,12 +1,23 @@
 import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { useRoute, useNavigation } from "@react-navigation/native";
 import { useLayoutEffect } from "react";
 import { Octicons } from "@expo/vector-icons";
 import { Ionicons } from "@expo/vector-icons";
 import { Fontisto } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
 import PropertyCard from "../components/PropertyCard";
+import {
+  BottomModal,
+  ModalContent,
+  ModalFooter,
+  ModalTitle,
+  SlideAnimation,
+} from "react-native-modals";
 const PlacesScreen = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectFilter, setSelectedFilter] = useState([]);
   const route = useRoute();
   const data = [
     {
@@ -469,6 +480,51 @@ const PlacesScreen = () => {
       ],
     },
   ];
+  const filters = [
+    {
+      id: "0",
+      filter: "cost:Low to High",
+    },
+    {
+      id: "1",
+      filter: "cost:High to Low",
+    },
+  ];
+  const searchPlaces = data?.filter((item) => item.place === route.params.place)
+  const [sortedData,setSortedData] = useState(data)
+  console.log(searchPlaces)
+  const compare  = (a,b) => {
+    if(a.newPrice > b.newPrice){
+      return -1;
+    }
+    if(a.newPrice < b.newPrice){
+      return 1;
+    }
+    return 0;
+  }
+  const comparison = (a,b) => {
+    if(a.newPrice < b.newPrice){
+      return -1;
+    }
+    if(a.newPrice > b.newPrice){
+      return 1;
+    }
+    return 0;
+
+  }
+  const applyFilter = (filter) => {
+    setModalVisible(false);
+    switch(filter){
+      case "cost:High to Low":
+        searchPlaces.map((val) => val.properties.sort(compare))
+        setSortedData(searchPlaces)
+        break;
+      case "cost:Low to High":
+        searchPlaces.map((val) => val.properties.sort(comparison))
+        setSortedData(searchPlaces)
+        break;
+    }
+  }
   const navigation = useNavigation();
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -501,7 +557,10 @@ const PlacesScreen = () => {
           backgroundColor: "white",
         }}
       >
-        <Pressable style={{ flexDirection: "row", alignItems: "center" }}>
+        <Pressable
+          onPress={() => setModalVisible(!modalVisible)}
+          style={{ flexDirection: "row", alignItems: "center" }}
+        >
           <Octicons name="arrow-switch" size={22} color="gray" />
           <Text style={{ fontSize: 15, fontWeight: 500, marginLeft: 8 }}>
             Sort
@@ -521,7 +580,7 @@ const PlacesScreen = () => {
         </Pressable>
       </Pressable>
       <ScrollView style={{ backgroundColor: "#f5f5f5" }}>
-        {data
+        {sortedData
           ?.filter((item) => item.place === route.params.place)
           .map((item) =>
             item.properties.map((property, index) => (
@@ -537,6 +596,76 @@ const PlacesScreen = () => {
             ))
           )}
       </ScrollView>
+      <BottomModal
+        onBackdropPress={() => setModalVisible(!modalVisible)}
+        swipeDirection={["up", "down"]}
+        swipeThreshold={200}
+        footer={
+          <ModalFooter>
+            <Pressable
+            onPress={() => applyFilter(selectFilter)}
+              style={{
+                paddingRight: 10,
+                marginLeft: "auto",
+                marginRight: "auto",
+                marginVertical: 10,
+                marginBottom:20
+              }}
+            >
+              <Text>Apply</Text>
+            </Pressable>
+          </ModalFooter>
+        }
+        modalTitle={<ModalTitle title="Sort and Filter" />}
+        modalAnimation={
+          new SlideAnimation({
+            slideFrom: "bottom",
+          })
+        }
+        onHardwareBackPress={() => setModalVisible(!modalVisible)}
+        visible={modalVisible}
+        onTouchOutside={() => setModalVisible(!modalVisible)}
+      >
+        <ModalContent style={{ width: "100%", height: 280 }}>
+          <View style={{ flexDirection: "row" }}>
+            <View
+              style={{
+                marginVertical: 10,
+                flex: 2,
+                height: 280,
+                borderRightWidth: 1,
+                borderColor: "#e0e0e0",
+              }}
+            >
+              <Text style={{ textAlign: "center" }}>Sort</Text>
+            </View>
+            <View style={{ flex: 3, margin: 10 }}>
+              {filters.map((item, index) => (
+                <Pressable
+                  onPress={() => setSelectedFilter(item.filter)}
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    marginVertical: 10,
+                  }}
+                  key={index}
+                >
+                  {selectFilter.includes(item.filter) ? (
+                    <FontAwesome name="circle" size={18} color="green" />
+                  ):(
+                      <Entypo name="circle" size={18} color="black" />
+                  )}
+                  <Text
+                    style={{ fontSize: 16, fontWeight: "500", marginLeft: 6 }}
+                  >
+                    {item.filter}
+                  </Text>
+                </Pressable>
+              ))}
+            </View>
+          </View>
+        </ModalContent>
+      </BottomModal>
     </View>
   );
 };
