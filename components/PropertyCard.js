23b6c0ae0d63d8704a981cd452b617abf06ef939
parshva555 <parshvadani@gmail.com>
2024-04-06 13:11:18 +0530

    Passing Props to Property Card to Display the Information about the hotels

---
+++ components/PropertyCard.js
@@ -0,0 +1,16 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PropertyCard = () => {
+  return (
+    <View>
+        <Pressable style={{flexDirection: 'row', margin:15, backgroundColor:"white"}}>
+
+        </Pressable>
+    </View>
+  )
+}
+
+export default PropertyCard
+
+const styles = StyleSheet.create({})
