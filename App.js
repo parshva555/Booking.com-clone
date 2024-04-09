e9594b41a69e236844df0f473ff9a4d5483c2a30
parshva555 <parshvadani@gmail.com>
2024-04-09 16:05:19 +0530

    Completed Bookings screen using redux

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import StackNavigator from './StackNavigator';
 import {ModalPortal} from 'react-native-modals'
+import {Provider} from 'react-redux'
+import store from './store';
 export default function App() {
   return (
     <>
+    <Provider store={store} >
     <StackNavigator/>
     <ModalPortal/>
+    </Provider>
     </>
   );
 }
