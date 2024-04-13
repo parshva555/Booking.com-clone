d5199649e3cf2f9e8dfcf90d14c5a4fcb7ca1208
Nihar Phansalkar <phansalkarnihar@gmail.com>
2024-04-13 17:17:02 +0000

    fetching user details from backend

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,10 +1,32 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const ProfileScreen = () => {
+  const [fullName, setFullName] = useState("");
+	const [email, setEmail] = useState("");
+
+  const fetchUserDetails = async () => {
+    const response = await fetch(`https://booking-backend-1-pmsm.onrender.com/api/users/user-details`, {
+      credentials: "include",
+    });
+
+    if (!response.ok) {
+      throw new Error("Error fetching user details");
+    }
+
+    const constructedFullName = `${response.firstName} ${response.lastName}`;
+		setFullName(constructedFullName);
+    setEmail(data.email);
+  };
+
+  useEffect(() => {
+    fetchUserDetails();
+  }, []);
+
   return (
     <View>
-      <Text>ProfileScreen</Text>
+      <Text>Name: {fullName}</Text>
+      <Text>Email: {email}</Text>
     </View>
   )
 }
